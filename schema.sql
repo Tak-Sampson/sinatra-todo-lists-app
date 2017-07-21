05faa15523d21e2ddde69f7dd97ac3dfd9deb2a2
Tak Sampson <tak.sampson@gmail.com>
2017-07-21 04:13:04 -0400

    Adding files to repo

---
+++ schema.sql
@@ -0,0 +1,13 @@
+-- schema for todo project relations
+
+CREATE TABLE lists (
+  id serial PRIMARY KEY,
+  name text UNIQUE NOT NULL
+);
+
+CREATE TABLE todos (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  completed boolean NOT NULL DEFAULT false,
+  list_id integer NOT NULL REFERENCES lists (id) ON DELETE CASCADE
+);
